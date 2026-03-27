573584f96abf00bcc0cef43b33eb92e05378fe5a
commSync <51642194+JM00NJ@users.noreply.github.com>
2026-03-27 14:37:39 +0300

    Update client.asm

--- client.asm
+++ client.asm
@@ -9,7 +9,7 @@
 ;                                   \|_________|                                   
 ; ===================================================================================
 ; Project      : Ghost-C2 (v3.0.2) - "The Invisible ICMP Phantom"
-; Author       : JM00NJ (https://github.com/JM00NJ) / https://netacoding.blogspot.com/
+; Author       : JM00NJ (https://github.com/JM00NJ) / https://netacoding.web.app/
 ; Architecture : x86_64 Linux (Pure Assembly, Libc-free)
 ; -----------------------------------------------------------------------------------
 ; Features:
